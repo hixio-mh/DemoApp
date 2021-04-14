d6aad545214a252fe1ebe8be6b9afdfd32adf296
Goooler <wangzongler@gmail.com>
2021-04-14 20:50:27 +0800

    简化 LoginActivity

--- login/src/main/java/io/goooler/demoapp/login/ui/LoginActivity.kt
+++ login/src/main/java/io/goooler/demoapp/login/ui/LoginActivity.kt
@@ -1,12 +1,6 @@
 package io.goooler.demoapp.login.ui
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import io.goooler.demoapp.login.R
 
-class LoginActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.login_activity)
-    }
-}
+class LoginActivity : AppCompatActivity(R.layout.login_activity)
