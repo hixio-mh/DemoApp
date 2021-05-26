f3c02578d501b4ab996cdb6ae3d89414fe12c985
Goooler <wangzongler@gmail.com>
2021-05-26 15:49:35 +0800

    添加 MainActivity

---
+++ main/src/main/java/io/goooler/demoapp/main/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package io.goooler.demoapp.main.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import io.goooler.demoapp.main.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
+}
